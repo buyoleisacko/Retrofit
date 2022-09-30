b0f56c8e61d8679ac07831adb48d4d2b29175337
Buyole Isacko <isackobuyole@gmail.com>
2022-09-30 12:31:08 +0300

    implemented the get my data

--- app/src/main/java/com/example/kotlinretrofit/ApiInterface.kt
+++ app/src/main/java/com/example/kotlinretrofit/ApiInterface.kt
@@ -1,8 +1,11 @@
 package com.example.kotlinretrofit
 
 import retrofit2.Call
+import retrofit2.http.GET
 
 interface ApiInterface {
+
+    @GET("posts")
 
     fun getData(): Call<List<MyDataItem>>
 
--- app/src/main/java/com/example/kotlinretrofit/MainActivity.kt
+++ app/src/main/java/com/example/kotlinretrofit/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.kotlinretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
 
+const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getMyData();
+    }
+
+    private fun getMyData() {
+        val retrofitBuilder= Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create)
+            .baseUrl(BASE_URL)
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getData()
     }
 }
